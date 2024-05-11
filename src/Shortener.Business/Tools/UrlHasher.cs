b6ecf543947b826e2fc6ed65d3479d376f21a055
Dmytro Mohylnytskyi <mhlntsk@gmail.com>
2024-05-11 15:04:01 +0300

    Added UrlHasher

---
+++ src/Shortener.Business/Tools/UrlHasher.cs
@@ -0,0 +1,26 @@
+﻿using Shortener.Business.Models;
+using System.Text;
+
+namespace Shortener.Business.Tools
+{
+    public static class UrlHasher
+    {
+        private static readonly Random random = new Random();
+        private const string AllPossibleCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        public static void GenerateShortUrl(this UrlShortenerModel url, int lengthOfShortUrl = 6)
+        {
+            if (lengthOfShortUrl <= 0)
+                throw new ArgumentException("Length of short url should be greater than zero.");
+
+            var shortUrlBuilder = new StringBuilder();
+
+            for (int i = 0; i < lengthOfShortUrl; i++)
+            {
+                shortUrlBuilder.Append(AllPossibleCharacters[random.Next(AllPossibleCharacters.Length)]);
+            }
+
+            url.ShortUrl = shortUrlBuilder.ToString();
+        }
+    }
+}
