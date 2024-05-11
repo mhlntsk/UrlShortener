104c683e6e55b9de7af270e26825501d8df0139e
Dmytro Mohylnytskyi <mhlntsk@gmail.com>
2024-05-11 11:49:07 +0300

    Added data-Entities to DAL

---
+++ src/Shortener.Data/Entities/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿using Shortener.Data.Interfaces;
+
+namespace Shortener.Data.Entities
+{
+    public class BaseEntity : IBaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ src/Shortener.Data/Entities/URL.cs
@@ -0,0 +1,13 @@
+﻿namespace Shortener.Data.Entities
+{
+    public class URL : BaseEntity
+    {
+        public string FullUrl { get; set; }
+        public string ShortUrl { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastAppeal {  get; set; }
+        public int NumberOfAppeals { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
