b138d03d6800be39a100deadb095e6cfdc580522
Dmytro Mohylnytskyi <mhlntsk@gmail.com>
2024-05-11 12:12:36 +0300

    Implemented repositories

---
+++ src/Shortener.Data/Repositories/UrlRepository.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shortener.Data.Data;
+using Shortener.Data.Entities;
+using Shortener.Data.Interfaces;
+using Shortener.Data.Validation;
+
+namespace Shortener.Data.Repositories
+{
+    public class UrlRepository : IRepository<URL>
+    {
+        private readonly ShortenerDbContext context;
+        public UrlRepository(ShortenerDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task AddAsync(URL entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            await context.URLs.AddAsync(entity);
+        }
+
+        public void Delete(URL entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            context.URLs.Remove(entity);
+        }
+
+        public async Task DeleteByIdAsync(int id)
+        {
+            var entity = await context.URLs.FindAsync(id);
+            if (entity != null)
+            {
+                context.URLs.Remove(entity);
+            }
+            else
+            {
+                throw new DatabaseDoesntContainException($"URL with id {id} not found");
+            }
+        }
+
+        public IQueryable<URL> GetAll()
+        {
+            return context.URLs;
+        }
+
+        public async Task<IEnumerable<URL>> GetAllAsync()
+        {
+            return await context.URLs.ToListAsync();
+        }
+
+        public async Task<URL?> GetByIdAsync(int id)
+        {
+            return await context.URLs.FindAsync(id);
+        }
+
+        public async Task Update(URL entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            var existingEntity = await GetByIdAsync(entity.Id);
+
+            if (existingEntity == null)
+            {
+                throw new DatabaseDoesntContainException($"URL with id {entity.Id} not found");
+            }
+
+            context.Entry(existingEntity).CurrentValues.SetValues(entity);
+        }
+    }
+}
---
+++ src/Shortener.Data/Repositories/UserRepository.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shortener.Data.Data;
+using Shortener.Data.Entities;
+using Shortener.Data.Interfaces;
+using Shortener.Data.Validation;
+
+namespace Shortener.Data.Repositories
+{
+    public class UserRepository : IRepository<User>
+    {
+        private readonly ShortenerDbContext context;
+        public UserRepository(ShortenerDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task AddAsync(User entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            await context.Users.AddAsync(entity);
+        }
+
+        public void Delete(User entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            context.Users.Remove(entity);
+        }
+
+        public async Task DeleteByIdAsync(int id)
+        {
+            var entity = await context.Users.FindAsync(id);
+            if (entity != null)
+            {
+                context.Users.Remove(entity);
+            }
+            else
+            {
+                throw new DatabaseDoesntContainException($"User with id {id} not found");
+            }
+        }
+
+        public IQueryable<User> GetAll()
+        {
+            return context.Users;
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            return await context.Users.ToListAsync();
+        }
+
+        public async Task<User?> GetByIdAsync(int id)
+        {
+            return await context.Users.FindAsync(id);
+        }
+
+        public async Task Update(User entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            var existingEntity = await GetByIdAsync(entity.Id);
+
+            if (existingEntity == null)
+            {
+                throw new DatabaseDoesntContainException($"User with id {entity.Id} not found");
+            }
+
+            context.Entry(existingEntity).CurrentValues.SetValues(entity);
+        }
+    }
+}
