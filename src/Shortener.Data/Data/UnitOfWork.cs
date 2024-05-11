d903fcdb6892e6aa9dd5a510fda0dd4567a13aa3
Dmytro Mohylnytskyi <mhlntsk@gmail.com>
2024-05-11 11:52:02 +0300

    Added UnitOfWork to DAL

---
+++ src/Shortener.Data/Data/UnitOfWork.cs
@@ -0,0 +1,45 @@
+﻿using Shortener.Data.Interfaces;
+using Shortener.Data.Repositories;
+
+namespace Shortener.Data.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ShortenerDbContext context;
+
+        private IUrlRepository? urlRepository;
+
+        private IUserRepository? userRepository;
+
+        public UnitOfWork(ShortenerDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IUrlRepository UrlRepository
+        {
+            get
+            {
+                if (urlRepository == null)
+                {
+                    urlRepository = new UrlRepository(context);
+                }
+                return urlRepository;
+            }
+        }
+
+        public IUserRepository UserRepository
+        {
+            get
+            {
+                if (userRepository == null)
+                {
+                    userRepository = new UserRepository(context);
+                }
+                return userRepository;
+            }
+        }
+
+        public async Task SaveAsync() => await context.SaveChangesAsync();
+    }
+}
