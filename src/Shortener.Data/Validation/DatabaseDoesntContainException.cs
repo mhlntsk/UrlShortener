9bbd1c61e97114fc5f1bbff774c971e99b25a50c
Dmytro Mohylnytskyi <mhlntsk@gmail.com>
2024-05-11 12:12:16 +0300

    Added DatabaseDoesntContainException

---
+++ src/Shortener.Data/Validation/DatabaseDoesntContainException.cs
@@ -0,0 +1,10 @@
+﻿namespace Shortener.Data.Validation
+{
+    [Serializable]
+    public class DatabaseDoesntContainException : Exception
+    {
+        public DatabaseDoesntContainException() : base() { }
+        public DatabaseDoesntContainException(string message) : base(message) { }
+        public DatabaseDoesntContainException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
