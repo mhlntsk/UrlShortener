17274252f46bcd4a9aee02e1f006a30dedf1079c
Dmytro Mohylnytskyi <mhlntsk@gmail.com>
2024-05-11 15:03:42 +0300

    Added ShortenerException

---
+++ src/Shortener.Business/Validation/ShortenerException.cs
@@ -0,0 +1,10 @@
+﻿namespace Shortener.Business.Validation
+{
+    [Serializable]
+    public class ShortenerException : Exception
+    {
+        public ShortenerException() : base() { }
+        public ShortenerException(string message) : base(message) { }
+        public ShortenerException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
