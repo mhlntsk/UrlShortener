27a0326f8924c1b005e356a0fc338de36bd1ce74
Dmytro Mohylnytskyi <mhlntsk@gmail.com>
2024-05-12 18:35:11 +0300

    Added AlreadyExistException to DAL

---
+++ src/Shortener.Data/Validation/AlreadyExistException.cs
@@ -0,0 +1,10 @@
+﻿namespace Shortener.Data.Validation
+{
+    [Serializable]
+    public class AlreadyExistException : Exception
+    {
+        public AlreadyExistException() : base() { }
+        public AlreadyExistException(string message) : base(message) { }
+        public AlreadyExistException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
